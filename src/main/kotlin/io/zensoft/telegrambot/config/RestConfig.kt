92b56fa79224e5afa1e7d5fad17318c5017ff75b
Evgeni <evgeni.efimenko@zensoft.io>
2019-03-07 12:33:29 +0300

    Improved restTemplate

--- src/main/kotlin/io/zensoft/telegrambot/config/RestConfig.kt
+++ src/main/kotlin/io/zensoft/telegrambot/config/RestConfig.kt
@@ -1,5 +1,7 @@
 package io.zensoft.telegrambot.config
 
+import io.zensoft.telegrambot.config.property.KickerApiProperties
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
@@ -8,9 +10,12 @@ import org.springframework.web.client.RestTemplate
  * @author Yauheni Efimenko
  */
 @Configuration
-class RestConfig {
+class RestConfig(
+        private val kickerApiProperties: KickerApiProperties
+) {
 
     @Bean
-    fun restTemplate(): RestTemplate = RestTemplate()
+    fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate =
+            restTemplateBuilder.rootUri(kickerApiProperties.host).build()
 
 }
